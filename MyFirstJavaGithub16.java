6c68cd1ed5a5f423700d0c3ce658e67fbe805cbf
Holycious <leonshanyoedhaadjie@outlook.com>
2023-09-08 14:53:17 +0700

    new line

--- MyFirstJavaGithub16.java
+++ MyFirstJavaGithub16.java
@@ -1,6 +1,7 @@
 public class MyFirstJavaGithub16 {
     public static void main(String[] args) {
         System.out.println("Hello from Github");
+        System.out.println("I am Leon");
     }
 
     
